740cad40860bea96784f0f3b826e7dd23be0115f
Tony Wahula <mawahula4@gmail.com>
2018-11-11 17:03:23 -0700

    service worker runs offline

--- sw.js
+++ sw.js
@@ -1,3 +1,41 @@
-self.addEventListener('fetch', function(event) {
-    console.log(event.request);
+const cacheFiles = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+  ];
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('restaurant-cache').then(function(cache) {
+     return cache.addAll(cacheFiles);
+   })
+ );
 });
+
+self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
+   
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+   });
+
+//    resource
+//    https://developers.google.com/web/fundamentals/codelabs/offline/
